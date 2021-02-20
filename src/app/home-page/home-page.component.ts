1cf861055fbe273cee51a03a0532546c1efe5967
unknown <fateh_djehinet@outlook.com>
2021-02-20 10:41:30 +0100

    hee

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
-  // displayedCol: string[] = [];
-  // data: MatTableDataSource<any> = new MatTableDataSource();
-
-  //TODO Profile Interface
+ 
   constructor() {}
   ngOnInit(): void {}
 }
